7398e4752f167be346b5918215ddf9a947186e4c
a <85395885+ArconRM@users.noreply.github.com>
2025-04-25 20:22:19 +0400

    Initial Commit

---
+++ AutoRepairShopPG/AutoRepairShopPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoRepairShopPGApp.swift
+//  AutoRepairShopPG
+//
+//  Created by Artemiy MIROTVORTSEV on 25.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AutoRepairShopPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
